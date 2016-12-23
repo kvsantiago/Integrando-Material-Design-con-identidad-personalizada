d02d576292ddce5931f03575f82c096fe1af8a40
kvsantiago <santiago-1994@live.com.ar>
2016-12-23 10:04:12 -0500

    TemaDesigner
    A continuacion presento mi tema en android

---
+++ Petagram/app/src/main/java/com/example/kvsantiago/petagram/DetalleMascota.java
@@ -0,0 +1,17 @@
+package com.example.kvsantiago.petagram;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Kv Santiago on 20/12/2016.
+ */
+public class DetalleMascota extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.mascota_detalle);
+
+    }
+}
---
+++ Petagram/app/src/main/java/com/example/kvsantiago/petagram/Listado_Mascotas.java
@@ -0,0 +1,16 @@
+package com.example.kvsantiago.petagram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Listado_Mascotas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listado__mascotas);
+
+        //todos los elementos que contiene mi interfaz grafica
+
+    }
+}
